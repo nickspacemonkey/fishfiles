70e6e586c7299f5e2ee8dd75f40488b5ab7229f8
nickspacemonkey@gmail.com <nick@nick-pc>
2025-04-09 10:57:45 +0100

    Combined sudo alias function with sudo !!

--- .config/fish/conf.d/config.fish
+++ .config/fish/conf.d/config.fish
@@ -48,16 +48,15 @@ function multicd
 end
 abbr --add dotdot --regex '^\.\.+$' --function multicd
 
-# Sudo aliases
 function sudo; command sudo -sE $argv; end
 
 # sudo !!
-function sudoredo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
+function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
     if test "$argv" = !!
         echo sudo $history[1]
-        eval command sudo $history[1]
+        eval command sudo -s $history[1]
     else
-        command sudo $argv
+        command sudo -s $argv
     end
 end
 
